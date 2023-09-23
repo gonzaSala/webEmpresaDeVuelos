0dc57b702fb0ba708e7a0809ace39dca19af789c
gonzaSala <gonzasalamendi@gmail.com>
2023-09-23 20:25:02 -0300

    agregando partes

---
+++ form.js
@@ -0,0 +1,31 @@
+document.getElementById('formVuelo').addEventListener('submit', function (e){
+    e.preventDefault()
+
+    let codOrigen = document.getElementById('origen').value
+    let codDestino = document.getElementById('destino').value
+    let idayvuelta = document.getElementById('idaYVuelta').value
+    let soloIda = document.getElementById('soloIda').value
+    let multiDestino = document.getElementById('multipleDestino').value
+    let cod = document.getElementById('clase').value
+    
+    let inputvuelta = document.getElementById('inputVuelta').value
+    let origen2 = document.getElementById('origen2').value
+    let destino2 = document.getElementById('destino2').value
+    let inputsalida = document.getElementById('inputSalida').value
+    let inputsalida2 = document.getElementById('inputSalida2').value
+
+    console.log(codigoorigen)
+                console.log(codigodestino)
+                console.log(idayvuelta)
+                console.log(soloida)
+                console.log(multipledestino)
+                console.log(inputida);
+                console.log(inputvuelta)
+                console.log(origen2)
+                console.log(destino2)
+                console.log(inputsalida)
+                console.log(inputsalida2)
+                console.log(codigo)
+
+})
+
